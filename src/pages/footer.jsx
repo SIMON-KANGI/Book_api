980b7899ddeb14fc0766b214edf3f2745202bd47
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-14 12:49:45 +0300

    footer

---
+++ src/pages/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='my-8'>
+      <h1 className='text-center font-bold text-stone-100'>&copy;2024 BookAI. All rights reserved</h1>
+      <h2 className='text-xl text-stone-400 font-bold text-center'>
+  Terms of Service&nbsp;&nbsp;Privacy Policy&nbsp;&nbsp;Contact Us
+</h2>
+
+    </div>
+  )
+}
+
+export default Footer
