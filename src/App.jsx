ea50af24c42e8f65508f58054635db87b0509c61
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-14 12:50:02 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Features from './pages/Features'
 import How from './pages/How'
 import RoadMap from './pages/RoadMap'
+import Footer from './pages/footer'
 function App() {
   return (
     <div className='main w-screen  flex flex-col'>
@@ -12,6 +13,8 @@ function App() {
      <Features/>
      <How/>
      <RoadMap/>
+     
+     <Footer/>
     </div>
   )
 }
