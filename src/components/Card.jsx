67e8732b67fdb9bf06eeb3699b9e082d53d3b12d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-14 17:51:02 +0300

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import colorPallete from '../context/colors'
 function Card({icon,title,description, containerStyles, iconStyles}) {
+  const theme=colorPallete()
   return (
     <div className={containerStyles}>
       <div className={iconStyles}>
     {icon}
       </div>
       <div>
-        <h3 className="text-2xl font-bold text-white mb-2">{title}</h3>
-        <p className="text-white text-md">{description}</p>
+        <h3 style={{ color: theme.color}}  className="text-2xl font-bold text-white mb-2">{title}</h3>
+        <p style={{ color: theme.color}}  className="text-white text-md">{description}</p>
       </div>
     </div>
   )
