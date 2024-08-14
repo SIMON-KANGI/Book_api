eaf8df8475bd22830378197d47611577b68ef028
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-14 19:07:25 +0300

    themecontext

--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -1,29 +1,28 @@
 import React, { createContext, useState, useContext } from 'react';
 
 const ThemeContext = createContext();
-const UpdateThemeContext =createContext()
+const UpdateThemeContext = createContext();
+
 export function UpdateTheme() { // Custom hook for toggle function
   return useContext(UpdateThemeContext);
 }
+
 function ThemeProvider({ children }) {
   const [darkTheme, setDarkTheme] = useState(true);
 
   const ToggleTheme = () => {
     setDarkTheme((prevTheme) => {
       const newTheme = !prevTheme;
-      
       localStorage.setItem('theme', newTheme ? 'dark' : 'light');
       return newTheme;
-      
     });
   };
 
   return (
     <ThemeContext.Provider value={darkTheme}>
-    <UpdateThemeContext.Provider value={ToggleTheme}>
+      <UpdateThemeContext.Provider value={ToggleTheme}>
         {children}
-    </UpdateThemeContext.Provider>
-    
+      </UpdateThemeContext.Provider>
     </ThemeContext.Provider>
   );
 }
