f20d0d84c5934259c8b548faa3ba1701736a4a99
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-15 12:01:04 +0300

    colors

--- src/context/colors.jsx
+++ src/context/colors.jsx
@@ -9,18 +9,20 @@ function colorPallete() {
         bg2:'rgba(32, 12, 62, 0.5)',
         bg3:"#1D2B53",
         bg4:"#2E4374",
+        bg5:"rgba(45, 16, 88, 0.5)",
         color: "#e0e0e0",
         color2:"#e0e0e0",
-        color3:"#212121",
+        color3:"#FFD700",
         color4:"#181818",
         color5:"#16161a"
     } : {
         bg: "#e0e0e0",
         bg2:'#F8EDED',
         bg3:"#F5E8C7",
-        color: "#141414",
+        bg5:"rgba(200, 200, 200, 0.5)",
+         color: "#141414",
         color2:'#33658a',
-        color3:'#e1e1e1',
+        color3:'#CA3E47',
         color4:"#e1e1e1",
         color5:"#33658a"
     };
