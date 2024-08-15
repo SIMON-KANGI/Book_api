aad48dea807d0c4fa062217716b7fd8d3f67c06c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-15 12:00:24 +0300

    footer

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   const theme= colorPallete()
   return (
     <div className='my-8'>
-      <h1 style={{ color: theme.color}}  className='text-center font-bold text-xl text-stone-100'>&copy;2024 BookAI. All rights reserved</h1>
-      <h2 style={{ color: theme.color2}} className='text-xl text-stone-400 font-bold text-center'>
+      <h1 style={{ color: theme.color}}  className='text-center font-bold lg:text-xl text-lg text-stone-100'>&copy;2024 BookAI. All rights reserved</h1>
+      <h2 style={{ color: theme.color2}} className='lg:text-xl text-lg text-stone-400 font-bold text-center'>
   Terms of Service&nbsp;&nbsp;Privacy Policy&nbsp;&nbsp;Contact Us
 </h2>
 
