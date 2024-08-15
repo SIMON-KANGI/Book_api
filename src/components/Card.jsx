4db06b0e94128c12fde937ba11cbe13b3654406c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-15 12:26:38 +0300

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import colorPallete from '../context/colors'
 function Card({icon,title,description, containerStyles, iconStyles}) {
   const theme=colorPallete()
   return (
-    <div className="flex  lg:items-center mx-4 lg:my-0 my-4">
-      <div className="bg-sky-500 lg:w-14 lg:h-14 w-16 h-16 rounded-full flex items-center justify-center mr-8">
+    <div className="flex flex-col sm:flex-row items-center p-4 lg:p-6 my-4 rounded-lg shadow-lg">
+      <div className=" shrink-1 bg-sky-500 lg:w-14 lg:h-14 w-16 h-16 rounded-full flex items-center justify-center mr-8">
     {icon}
       </div>
-      <div>
+      <div className="text-center sm:text-left">
         <h3 style={{ color: theme.color}}  className="lg:text-2xl text-xl font-bold text-white mb-2">{title}</h3>
         <p style={{ color: theme.color}}  className="text-white text-md">{description}</p>
       </div>
