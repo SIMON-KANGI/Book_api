04846753a36c93a58a5570d528631302f24f5d55
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-15 12:02:11 +0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import ThemeProvider from './context/themeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
-  <ThemeProvider>
+  <ChakraProvider>
+    <ThemeProvider>
      <App /> 
   </ThemeProvider>
+  </ChakraProvider>
+  
    
   </BrowserRouter>
    
