2fb1232af6baf98f8889eee11b1965e8d9655129
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-15 11:59:54 +0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,16 +19,16 @@ function Home() {
         with <br />
         BookAI
       </h1>
-      <div className='my-8'>
-        <p style={{ color: theme.color2}} className='text-xl text-stone-300 text-center'>Unleash the power of AI to create captivating books in minutes.</p>
-        <p style={{ color: theme.color2}} className='text-xl text-stone-300 text-center'>Download, Distribute wherever you want. Generate unlimited free <br/>books</p>
+      <div className='my-8 lg:mx-0 mx-6'>
+        <p style={{ color: theme.color2}} className='lg:text-xl text-lg text-wrap text-stone-300 lg:text-center'>Unleash the power of AI to create captivating books in minutes.</p>
+        <p style={{ color: theme.color2}} className='lg:text-xl text-lg text-stone-300 lg:text-center'>Download, Distribute wherever you want. Generate unlimited free <br/>books</p>
       </div>
       <div className='justify-center items-center my-8  flex'>
         <button style={{ color: theme.color}} className='bg-gradient-to-r from-blue-500 to-blue-300 px-8 py-3 mx-2 rounded-full text-xl text-white font-bold items-center'>Get Started</button>
         <button style={{ color: theme.color}}   className='bg-gradient-to-r from-blue-500 to-blue-300 px-8 py-3 rounded-full text-xl text-white font-bold  items-center'>Get Api Access</button>
       </div>
-      <h1 className='text-amber-500 py-16 text-center'>Attention:TryBookAI has been acquired by Indicus AI. Indicus LLM will be used for text and image generation</h1>
-      <div className=' book2 py-8 px-6  relative rounded-md w-3/5 h-fit   mx-auto '>
+      <h1 style={{color:theme.color3}} className=' py-16 font-bold text-center'>Attention:TryBookAI has been acquired by Indicus AI. Indicus LLM will be used for text and image generation</h1>
+      <div style={{backgroundColor:theme.bg5}} className=' book2 py-8 px-6  relative rounded-md lg:w-3/5 w-full h-fit   mx-auto '>
          <div className="w-100 h-100 ">
          <img src="book2.png" alt="book2" className='rounded-lg transition-transform transform hover:scale-105' /></div>
          <div className='paradox top-6 absolute bg-slate-300 py-2 pl-10 pr-2 rounded-lg transition-transform transform hover:scale-105'>
